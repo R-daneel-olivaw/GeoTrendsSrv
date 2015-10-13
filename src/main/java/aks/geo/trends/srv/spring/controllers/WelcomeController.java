c29b5433b0dd67898bfdae365f26e72b99dca1e1
Akshat Kumar Vaish desktop <akshatvaish.public@gmail.com>
2015-10-13 16:27:01 -0400

    linked spring controllers to the keyword fetching logic

--- src/main/java/aks/geo/trends/srv/spring/controllers/WelcomeController.java
+++ src/main/java/aks/geo/trends/srv/spring/controllers/WelcomeController.java
@@ -1,21 +1,30 @@
 package aks.geo.trends.srv.spring.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+
+import aks.geo.trends.srv.TrendingByCountry;
 
 @Controller
 @RequestMapping("/test")
 public class WelcomeController {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView welcomeScreen()
-	{
-		ModelAndView mv = new ModelAndView("test");
+	public String welcomeScreen(Model model) {
+
+		TrendingByCountry trendsCountry = new TrendingByCountry();
+		List<String> trendingIndia = trendsCountry.fetchTrendingForIndia();
 		
-		return mv;
+		System.out.println("Trending for India : "+trendingIndia);
 		
+		model.addAttribute("trending_india", trendingIndia);
+
+		return "test";
+
 	}
 
 }
--- src/main/webapp/WEB-INF/views/test.jsp
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+<%@ page contentType="text/html; charset=utf-8" language="java"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +8,12 @@
 </head>
 <body>
 	<h2>test controller is working</h2>
+
+	<ol>
+		<c:forEach items="${trending_india}" var="keyword">
+			<li>${keyword}</li>
+		</c:forEach>
+	</ol>
+
 </body>
 </html>
