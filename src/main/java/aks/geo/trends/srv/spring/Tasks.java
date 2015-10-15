570eedef73b7e4f62c881051486222fdb9acc219
Akshat Kumar Vaish desktop <akshatvaish.public@gmail.com>
2015-10-15 14:08:05 -0400

    added dependencies for slf4j and log4j2 added logging using slf4j to Tasks class

--- src/main/java/aks/geo/trends/srv/spring/Tasks.java
+++ src/main/java/aks/geo/trends/srv/spring/Tasks.java
@@ -9,8 +9,13 @@ import org.springframework.stereotype.Component;
 import aks.geo.trends.srv.TrendingByCountry;
 import aks.geo.trends.srv.spring.services.KeywordService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 public class Tasks {
+	
+	private final Logger logger = LoggerFactory.getLogger(Tasks.class);
 	
 	@Autowired
 	KeywordService keywordService;
@@ -18,23 +23,35 @@ public class Tasks {
 	@Scheduled(fixedDelay=60000)
 	public void updateTrendingKeywordsIndia()
 	{
+		logger.info("Requesting keywords for India");
+		
 		TrendingByCountry trendsCountry = new TrendingByCountry();
 		List<String> trendingIndia = trendsCountry.fetchTrendingForIndia();
 		
-		System.out.println("Trending for India : "+trendingIndia);
+		logger.debug("Trending for India : "+trendingIndia);
 		
+		logger.trace("Updating DB for keywords from India");
 		keywordService.updateDatabase(trendingIndia, "IN");
+		logger.trace("completed update of DB for keywords from India");
+		
+		logger.info("Completed requesting keywords for India");
 	}
 	
 	@Scheduled(fixedDelay=60000)
 	public void updateTrendingKeywordsUS()
 	{
+		logger.info("Requesting keywords for US");
+		
 		TrendingByCountry trendsCountry = new TrendingByCountry();
-		List<String> trendingIndia = trendsCountry.fetchTrendingForUS();
+		List<String> trendingUS = trendsCountry.fetchTrendingForUS();
 		
-		System.out.println("Trending for US : "+trendingIndia);
+		logger.debug("Trending for US : "+trendingUS);
 		
-		keywordService.updateDatabase(trendingIndia, "US");
+		logger.trace("Updating DB for keywords from US");
+		keywordService.updateDatabase(trendingUS, "US");
+		logger.trace("completed update of DB for keywords from US");
+		
+		logger.info("Completed requesting keywords for US");
 	}
 
 }
